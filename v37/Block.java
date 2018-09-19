59374b744be94f451894ff8a0d87861b7e362c66
JaoK <jjaokk@gmail.com>
2018-09-19 12:32:59 +0200

    final map v37

--- v37/Block.java
+++
@@ -1,8 +0,0 @@
-import javafx.scene.Group;
-
-public class Block extends Group{
-	
-	public static double SIZE = 25;
-
-	
-}
--- v37/map/labyrint.java
+++ v37/map/labyrint.java
@@ -1,23 +1,28 @@
+package map;
+
 import java.io.File;
 
+import blocks.Block;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-public class maze extends Application{
+public class labyrint extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		Map map = MapInterpreter.interpretMap(new File("map.txt"));
-		Scene scene = new Scene(map,map.getWidth(),map.getHeight());
-		
+		Map map = MapInterpreter.interpretMap(new File("v37/map.txt"));
+
+		Scene scene = new Scene(map, map.getWidth(), map.getHeight());
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
-	
 	}
-	
+
 	public static void main(String[] args) {
 		launch();
 	}
