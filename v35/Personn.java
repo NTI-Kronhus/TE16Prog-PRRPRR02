dc357d73f273f21bc2ca85533bd09b20fc1cdfe0
JaoK <jjaokk@gmail.com>
2018-10-10 13:43:09 +0200

    v35 name change

--- v35/Personn.java
+++ v35/Personn.java
@@ -1,11 +1,11 @@
 
-public class Person <T> {
+public class Personn <T> {
 
 	private int age;
 	private String name;
 	private T favoritsak;
 	
-	public  Person(String name, int age, T favoritsak){
+	public  Personn(String name, int age, T favoritsak){
 		this.age = age;
 		this.name = name;
 		this.favoritsak = favoritsak;		
