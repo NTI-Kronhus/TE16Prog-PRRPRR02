e57ca2b670fe72bfeec6a3db9e0006a83935465e
JaoK <jjaokk@gmail.com>
2018-10-19 12:32:03 +0200

    imageview

---
+++ v42/image/Pawn.java
@@ -0,0 +1,20 @@
+package image;
+
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Pawn extends Group{
+	
+	public Pawn(){
+		
+		Image image = new Image("pawn.png");
+		ImageView iw = new ImageView(image);
+		iw.setFitWidth(100);
+		iw.setFitHeight(100);
+		this.getChildren().add(iw);
+		
+		
+	}
+
+}
